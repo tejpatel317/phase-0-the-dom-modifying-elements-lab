1ecbdbaba8f510a5a4b162ac993a4467e81d1526
Tej Patel <tejpatel317@gmail.com>
2022-05-04 19:57:00 -0400

    commit tree 8b71065853ea912a9cc6ca0b93666b68e1ef686d

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+//Remove ID main
+
+
+const element1 = document.getElementById("main");
+element1.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion"
+document.body.append(newHeader);
